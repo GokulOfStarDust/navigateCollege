cdf516ab5eb057f7103715ab784ec34937e0bbe1
GokulofStarDust <gokulgopalan18@gmail.com>
2025-09-14 19:53:37 +0530

    CHANGED default start position

--- src/pages/context/LocRouteProvider.jsx
+++ src/pages/context/LocRouteProvider.jsx
@@ -42,7 +42,7 @@ function Route({start, end}) {
 function LocRouteProvider({children}) {
 
     const [userPosition, setUserPosition] = useState(null);
-    const [startEndPos, setStartEndPos] = useState({start: { lat: 11.0324860, lng: 77.0329760 }, end: { lat: 11.032390, lng: 77.033279 }});
+    const [startEndPos, setStartEndPos] = useState({start: { lat: 11.034783, lng: 77.033802 }, end: { lat: 11.032390, lng: 77.033279 }});
     const [mapInstance, setMapInstance] = useState(null);
 
     const flyToUserPosition = () => {
